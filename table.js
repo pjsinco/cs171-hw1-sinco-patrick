a904ba54602852fcacab1d6c1ba2c516e58e38ed
Patrick Sinco <psinco@gmail.com>
2014-02-08 20:06:26 -0600

    mostly answer problem 2, #3; but unable to make linebreaks

--- table.js
+++ table.js
@@ -6,11 +6,25 @@
     'unemp_states_us_nov_2013.tsv', 
     'text/plain', 
     function(data) {
+      var title = 'Unemployment Rates for States\n';
+      title += 'Monthly Rankings\n';
+      title += 'Seasonally Adjusted\n';
+      title += 'Nov. 2013';
+
+      d3.select('body').append('h1')
+        .text('Unemployment Rates for States');
       var parsed = d3.tsv.parseRows(data);
       var table = d3.select('body').append('table');
+      var caption = d3.select('table').append('caption')
+      
       var thead = table.append('thead').append('tr');
       var tbody = table.append('tbody');
 
+      d3.select('caption')
+        .text(title);
+
+      console.log(title);
+ 
       thead.selectAll('th')
         .data(parsed[0])
         .enter().append('th')
@@ -18,7 +32,7 @@
             return d;
           });
 
-      parsed.shift(); // remove header
+      parsed.shift(); // remove header row
 
       var rows = tbody.selectAll('tr')
         .data(parsed)
